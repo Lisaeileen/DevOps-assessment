49ddb5ecd98a2da6a93a7494e9fafc6a3f1c3281
Lisa-Eileen <ucheodinukwe@yahoo.com>
2025-08-12 13:10:36 -0400

    Update variables.tf

--- terraform/modules/variables.tf
+++ terraform/modules/variables.tf
@@ -1,1 +1,22 @@
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "lisa-env" {
+  description = "environment (development, staging, production)"
+  type        = string
+}
+
+variable "lisa-s3-logs" {
+  description = "s3 bucket to store our site"
+  type        = string
+  default     = "fullstack-logs-lisa"
+}
+
+variable "lisa-s3-site" {
+  description = "s3 bucket to store our site"
+  type        = string
+  default     = "fullstack-site-lisa"
+}
