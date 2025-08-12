0df9b56745e99f0cc7f06d4027bbeb977c2d4d79
Lisa-Eileen <ucheodinukwe@yahoo.com>
2025-08-12 04:09:40 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,20 @@
-# In this file put the variables related to the deployment
-variable "variable_name" {
-    type = "type",
-    description = "Description"
+variable "aws_region" {
+    description = "AWS region to deploy resource"
+    type = string
+    default = "us-east-1"
+}
+
+variable "lisa-environment" {
+    description = "environment (development, staging, production)"
+    type = string
+}
+
+variable "lisa-s3-bucket-log" {
+    description = "s3 bucket to store my log"
+    type = string 
+}
+
+variable "lisa-s3-bucket-site" {
+    description = "s3 bucket to store my site"
+    type = string 
 }
